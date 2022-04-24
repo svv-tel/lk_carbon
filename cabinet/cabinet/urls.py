315a71e1d5c4d13ee2cd20cb9647a8773fa8dc5b
Владимир Самошкин <svv-tel@yandex.ru>
2022-04-25 00:46:08 +0300

    Создание структуры проекта_2

--- cabinet/cabinet/urls.py
+++ cabinet/cabinet/urls.py
@@ -1,4 +1,4 @@
-"""lk_carbonsoft URL Configuration
+"""cabinet URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- cabinet/manage.py
+++ cabinet/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lk_carbonsoft.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cabinet.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
